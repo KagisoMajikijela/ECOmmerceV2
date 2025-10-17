546054d632202666c1371aa829385da1eddc07af
Kagiso Majikijela <majikijelaclinton@gmail.com>
2025-10-18 00:41:47 +0200

    Added login FUnctionality

---
+++ Functionality/login.js
@@ -0,0 +1,31 @@
+// let users = [
+//     [username ,'usernavevalue', userprofile ,'userimg'],
+//     [username,'usernavevalue', userprofile ,'userimg']
+// ]
+//functtion to chech login username//
+const loggedin =false
+const Sloogedin =true
+const username = document.getElementsByName('username')
+const password =document.getElementsByName('password')
+const loginbtn =document.getElementById('login-btn')
+loginbtn.addEventListener('click',login)
+
+
+function login(){
+    
+    //Error in code to be fixed 
+    if(!username==username || !password ==password)
+        window.alert('Wrong Loggin credentials')
+    else {
+        window.alert(`Welcome back ${username}`)
+        setTimeout(3000)
+        window.location.href='home.html'
+    }
+
+    if(Sloogedin)
+        window.location.href='home.html'
+
+    while(loggedin)
+        window.alert('You are not logged in')
+   
+}
